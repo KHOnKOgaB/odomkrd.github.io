123bc5a8b04e80e919d69c52a459d7d8d31a8ebc
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2019-04-29 12:24:33 +0300

    script

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,34 @@
  // ---------- BURGER KING MENU ----------
-    $('#burger-king').click(function(event) {
-        $('.top-menu').toggleClass('active')
-    })
-
-    $('.events__list').slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		// centerMode: true,
-		// variableWidth: true,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		responsive: [
-	    {
-	      breakpoint: 824,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2,
-	        infinite: true,
-	        dots: true
-	      }
-	    },
-	    {
-	      breakpoint: 600,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }],
-		prevArrow: $('.prev-arr'),
-		nextArrow: $('.next-arr')
-	});
+ $('#burger-king').click(function(event) {
+     $('.top-menu').toggleClass('active')
+ })
+ $(document).ready(function() {
+     $('.events__list').slick({
+         infinite: true,
+         slidesToShow: 3,
+         slidesToScroll: 1,
+         // centerMode: true,
+         // variableWidth: true,
+         autoplay: true,
+         autoplaySpeed: 2000,
+         responsive: [{
+                 breakpoint: 824,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 2,
+                     infinite: true,
+                     dots: true
+                 }
+             },
+             {
+                 breakpoint: 600,
+                 settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 1
+                 }
+             }
+         ],
+         prevArrow: $('.prev-arr'),
+         nextArrow: $('.next-arr')
+     });
+ });
