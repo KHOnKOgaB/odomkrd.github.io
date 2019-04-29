46b37f0369cdec3dee859526084b9d69c664fec5
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2019-04-29 13:17:43 +0300

    burger

--- src/js/main.js
+++ src/js/main.js
@@ -2,32 +2,3 @@
     $('#burger').click(function(event) {
         $('.menu').toggleClass('active')
     })
-
-    $('.carousel').slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		// centerMode: true,
-		// variableWidth: true,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		responsive: [
-	    {
-	      breakpoint: 824,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2,
-	        infinite: true,
-	        dots: true
-	      }
-	    },
-	    {
-	      breakpoint: 600,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }],
-		prevArrow: $('.prev-arr'),
-		nextArrow: $('.next-arr')
-	});
